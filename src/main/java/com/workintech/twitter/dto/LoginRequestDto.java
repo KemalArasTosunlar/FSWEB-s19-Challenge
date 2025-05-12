7d6500542466202f479e994db280fa8d6795fbc5
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-05-12 16:45:38 +0300

    Implement LoginRequestDto and LoginResponseDto with validation and token handling

--- src/main/java/com/workintech/twitter/dto/LoginRequestDto.java
+++ src/main/java/com/workintech/twitter/dto/LoginRequestDto.java
@@ -1,5 +1,30 @@
 package com.workintech.twitter.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequestDto {
-    // Fields and methods to be implemented
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+    // Getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/workintech/twitter/dto/LoginResponseDto.java
+++ src/main/java/com/workintech/twitter/dto/LoginResponseDto.java
@@ -1,5 +1,27 @@
 package com.workintech.twitter.dto;
 
 public class LoginResponseDto {
-    // Fields and methods to be implemented
+
+    private String token;
+    private String tokenType = "Bearer";
+
+    public LoginResponseDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 }
